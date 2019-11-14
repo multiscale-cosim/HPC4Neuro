a1bd101af590019ad7313dfee5e9833adb6eb25a
Fahad Khalid <f.khalid@fz-juelich.de>
2019-11-14 15:15:40 +0100

    Added further tests.

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -72,8 +72,10 @@ class TestExceptionHandling:
             get_rank_local_filenames('./this_is_not_a_dir')
 
     def test_non_iterable_error(self, tmpdir):
+        num_ranks = MPI.COMM_WORLD.Get_size()
+
         # Create file in a temporary directory
-        filenames = ['file_1.txt', 'file_2.txt', 'file_3.txt', 'file_4.txt']
+        filenames = generate_filenames(num_files=num_ranks)
         for filename in filenames:
             with open(os.path.join(tmpdir, filename), 'w') as f:
                 f.write(filename)
@@ -87,8 +89,10 @@ class TestExceptionHandling:
             get_rank_local_filenames(tmpdir)
 
     def test_non_sized_error(self, tmpdir):
+        num_ranks = MPI.COMM_WORLD.Get_size()
+
         # Create file in a temporary directory
-        filenames = ['file_1.txt', 'file_2.txt', 'file_3.txt', 'file_4.txt']
+        filenames = generate_filenames(num_files=num_ranks)
         for filename in filenames:
             with open(os.path.join(tmpdir, filename), 'w') as f:
                 f.write(filename)
@@ -102,7 +106,6 @@ class TestExceptionHandling:
             get_rank_local_filenames(tmpdir)
 
     def test_size_mismatch_error(self, tmpdir):
-        # Create file in a temporary directory
         num_ranks = MPI.COMM_WORLD.Get_size()
         filenames = generate_filenames(num_ranks - 1)
 
@@ -115,3 +118,51 @@ class TestExceptionHandling:
 
         with pytest.raises(DataDistributionError):
             get_rank_local_filenames(tmpdir)
+
+
+class TestDistribution:
+    def test_distribution_happened(self, tmpdir):
+        rank = MPI.COMM_WORLD.Get_rank()
+        num_ranks = MPI.COMM_WORLD.Get_size()
+
+        filenames = generate_filenames(num_files=num_ranks)
+
+        for filename in filenames:
+            with open(os.path.join(tmpdir, filename), 'w') as f:
+                f.write(filename)
+
+        dist_decorator = DataDistributor(MPI.COMM_WORLD, shutdown_on_error=False)
+        get_rank_local_filenames = dist_decorator(os.listdir)
+
+        updated_filenames = get_rank_local_filenames(tmpdir)
+
+        assert len(updated_filenames) >= 1
+
+    def test_distributed_items_are_disjoint(self, tmpdir):
+        # rank = MPI.COMM_WORLD.Get_rank()
+        num_ranks = MPI.COMM_WORLD.Get_size()
+
+        filenames = generate_filenames(num_files=num_ranks)
+
+        for filename in filenames:
+            with open(os.path.join(tmpdir, filename), 'w') as f:
+                f.write(filename)
+
+        dist_decorator = DataDistributor(MPI.COMM_WORLD, shutdown_on_error=False)
+        get_rank_local_filenames = dist_decorator(os.listdir)
+
+        updated_filenames = get_rank_local_filenames(tmpdir)
+
+        gathered_filenames = MPI.COMM_WORLD.allgather(updated_filenames)
+
+        assert len(gathered_filenames) == num_ranks
+
+        count = 0
+        for f_names in gathered_filenames:
+            count += len(f_names)
+
+        unique_filenames = set()
+        for f_names in gathered_filenames:
+            unique_filenames.add(*f_names)
+
+        assert count == len(unique_filenames)
