212aec9f1141ab28560e2e25a5af3e5f8bbc1969
Fahad Khalid <f.khalid@fz-juelich.de>
2019-11-15 09:31:52 +0100

    Refactoring test code.

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -1,6 +1,8 @@
 
 import os
+import tempfile
 import functools
+import itertools
 
 import pytest
 from mpi4py import MPI
@@ -8,15 +10,9 @@ from mpi4py import MPI
 from slns.distribution import DataDistributor
 from slns.errors import DataDistributionError
 
-# TODO: We can start by testing exception handling.
-#       1. Exception thrown by data loader
-#           i. Define the data loader
-#           ii. Pass path to a non-existent directory
-#           iii. Check exception is raised with shutdown_on_error=False
-#       2. Check iterable
-#       3. Check sized
-#       4. Check No. of items
-#       How can one test proper execution with shutdown_on_error=True ??
+
+rank = MPI.COMM_WORLD.Get_rank()
+num_ranks = MPI.COMM_WORLD.Get_size()
 
 
 class SizedIterable:
@@ -46,38 +42,14 @@ class SizedNotIterable:
         return len(self._iterable)
 
 
-def make_sized_iterable(func):
+def change_result_type(func, custom_type):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
 
-        sized_iterable = SizedIterable(result)
+        result_with_new_type = custom_type(result)
 
-        return sized_iterable
-
-    return wrapper
-
-
-def make_not_sized(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        sized_iterable = func(*args, **kwargs)
-
-        iterable = IterableNotSized(sized_iterable)
-
-        return iterable
-
-    return wrapper
-
-
-def make_not_iterable(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        sized_iterable = func(*args, **kwargs)
-
-        sized = SizedNotIterable(sized_iterable)
-
-        return sized
+        return result_with_new_type
 
     return wrapper
 
@@ -86,64 +58,123 @@ def generate_filenames(num_files):
     return [f'file_{i}.txt' for i in range(num_files)]
 
 
+def generate_files(target_dir, num_files):
+    if rank == 0:
+        filenames = generate_filenames(num_files)
+
+        for filename in filenames:
+            with open(os.path.join(target_dir, filename), 'w') as f:
+                f.write(filename)
+
+
+def get_decorated_func(func, shutdown_on_error):
+    decorator = DataDistributor(
+        MPI.COMM_WORLD, shutdown_on_error=shutdown_on_error)
+
+    return decorator(func)
+
+
 class TestExceptionHandling:
-    def test_data_loader_error(self):
-        dist_decorator = DataDistributor(MPI.COMM_WORLD, shutdown_on_error=False)
-        get_rank_local_filenames = dist_decorator(os.listdir)
+    def test_error_from_data_loader(self):
+        """
+        An error thrown by the decorated data loader function should
+        be raised as slns.errors.DataDistributionError when
+        automatic shutdown is not requested.
+        """
+
+        # Decorate the data loader with distribution functionality
+        decorated_func = get_decorated_func(func=os.listdir, shutdown_on_error=False)
+
+        # Distribution error should be thrown because the loader
+        # function should generated an error when trying to list
+        # a non-existent directory.
+        with pytest.raises(DataDistributionError):
+            decorated_func('./non-existent-dir')
+
+    def test_error_for_non_iterable(self, tmpdir):
+        """
+        When the function to be decorated returns an object that is
+        not iterable, the slns.errors.DataDistributionError should
+        be raised (when automatic shutdown is not requested).
+
+        :param tmpdir: Temporary directory created by Pytest fixture.
+        """
+
+        # Generate files in a temporary directory
+        generate_files(tmpdir, num_files=num_ranks)
+
+        # Create a data loader function that is iterable but not sized
+        data_loader = change_result_type(os.listdir, SizedNotIterable)
+
+        # Decorate the data loader with distribution functionality
+        decorated_func = get_decorated_func(data_loader, shutdown_on_error=False)
 
         with pytest.raises(DataDistributionError):
-            get_rank_local_filenames('./this_is_not_a_dir')
+            decorated_func(tmpdir)
 
-    def test_non_iterable_error(self, tmpdir):
-        num_ranks = MPI.COMM_WORLD.Get_size()
+    def test_error_for_non_sized(self, tmpdir):
+        """
+        When the function to be decorated returns an object that is
+        not sized, the slns.errors.DataDistributionError should
+        be raised (when automatic shutdown is not requested).
 
-        # Create file in a temporary directory
-        filenames = generate_filenames(num_files=num_ranks)
-        for filename in filenames:
-            with open(os.path.join(tmpdir, filename), 'w') as f:
-                f.write(filename)
+        :param tmpdir: Temporary directory created by Pytest fixture.
+        """
 
-        callable_ = make_not_sized(os.listdir)
+        # Generate files in a temporary directory
+        generate_files(tmpdir, num_files=num_ranks)
 
-        dist_decorator = DataDistributor(MPI.COMM_WORLD, shutdown_on_error=False)
-        get_rank_local_filenames = dist_decorator(callable_)
+        # Create a data loader function that is iterable but not sized
+        data_loader = change_result_type(os.listdir, IterableNotSized)
 
-        with pytest.raises(DataDistributionError):
-            get_rank_local_filenames(tmpdir)
-
-    def test_non_sized_error(self, tmpdir):
-        num_ranks = MPI.COMM_WORLD.Get_size()
-
-        # Create file in a temporary directory
-        filenames = generate_filenames(num_files=num_ranks)
-        for filename in filenames:
-            with open(os.path.join(tmpdir, filename), 'w') as f:
-                f.write(filename)
-
-        callable_ = make_not_iterable(os.listdir)
-
-        dist_decorator = DataDistributor(MPI.COMM_WORLD, shutdown_on_error=False)
-        get_rank_local_filenames = dist_decorator(callable_)
+        # Decorate the data loader with distribution functionality
+        decorated_func = get_decorated_func(data_loader, shutdown_on_error=False)
 
         with pytest.raises(DataDistributionError):
-            get_rank_local_filenames(tmpdir)
+            decorated_func(tmpdir)
 
-    def test_size_mismatch_error(self, tmpdir):
-        num_ranks = MPI.COMM_WORLD.Get_size()
-        filenames = generate_filenames(num_ranks - 1)
+    def test_error_for_size_mismatch(self, tmpdir):
+        """
+        If the number of items to be distributed returned by the
+        decorated function is less the number of MPI ranks, the
+        slns.errors.DataDistributionError should be raised (when
+        automatic shutdown is not requested).
 
-        for filename in filenames:
-            with open(os.path.join(tmpdir, filename), 'w') as f:
-                f.write(filename)
+        :param tmpdir: Temporary directory created by Pytest fixture.
+        """
 
-        dist_decorator = DataDistributor(MPI.COMM_WORLD, shutdown_on_error=False)
-        get_rank_local_filenames = dist_decorator(os.listdir)
+        # Generate files in a temporary directory
+        generate_files(tmpdir, num_files=num_ranks - 1)
+
+        # Decorate the data loader with distribution functionality
+        decorated_func = get_decorated_func(os.listdir, shutdown_on_error=False)
 
         with pytest.raises(DataDistributionError):
-            get_rank_local_filenames(tmpdir)
+            decorated_func(tmpdir)
 
 
-class TestDistribution:
+@pytest.fixture(scope='class')
+def distributed_filenames():
+    # Create a temporary data directory
+    with tempfile.TemporaryDirectory(prefix='slns_test_') as data_dir:
+        # Generate files in a temporary directory
+        generate_files(data_dir, num_files=num_ranks)
+
+        # Decorate the data loader with distribution functionality
+        decorated_func = get_decorated_func(os.listdir, shutdown_on_error=False)
+
+        # Call the decorated function to get the list of rank-local
+        # filenames
+        rank_local_filenames = decorated_func(data_dir)
+
+        # Collect a list of all lists of filenames from all ranks
+        gathered_filename_lists = MPI.COMM_WORLD.allgather(rank_local_filenames)
+
+        yield gathered_filename_lists, data_dir
+
+
+# TODO: Test distribution of numpy arrays ...
+class TestFilenamesDistribution:
     def test_distribution_happened(self, tmpdir):
         rank = MPI.COMM_WORLD.Get_rank()
         num_ranks = MPI.COMM_WORLD.Get_size()
@@ -170,40 +201,43 @@ class TestDistribution:
             with open(os.path.join(tmpdir, filename), 'w') as f:
                 f.write(filename)
 
-        callable_ = make_sized_iterable(os.listdir)
+        # data_loader = make_sized_iterable(os.listdir)
+        data_loader = change_result_type(os.listdir, SizedIterable)
 
         dist_decorator = DataDistributor(MPI.COMM_WORLD, shutdown_on_error=False)
-        get_rank_local_filenames = dist_decorator(callable_)
+        get_rank_local_filenames = dist_decorator(data_loader)
 
         updated_filenames = get_rank_local_filenames(tmpdir)
 
         assert len(updated_filenames) >= 1
 
-    def test_distributed_items_are_disjoint(self, tmpdir):
-        # rank = MPI.COMM_WORLD.Get_rank()
-        num_ranks = MPI.COMM_WORLD.Get_size()
+    def test_all_ranks_get_items(self, distributed_filenames):
+        gathered_filename_lists, _ = distributed_filenames
 
-        filenames = generate_filenames(num_files=num_ranks)
+        # The No. of lists returned should equal the No. of ranks
+        assert len(gathered_filename_lists) == num_ranks
 
-        for filename in filenames:
-            with open(os.path.join(tmpdir, filename), 'w') as f:
-                f.write(filename)
+        # Every sublist must contain at least one element
+        assert all(filenames for filenames in gathered_filename_lists)
 
-        dist_decorator = DataDistributor(MPI.COMM_WORLD, shutdown_on_error=False)
-        get_rank_local_filenames = dist_decorator(os.listdir)
+    def test_ranks_get_disjoint_subsets(self, distributed_filenames):
+        gathered_filename_lists, _ = distributed_filenames
 
-        updated_filenames = get_rank_local_filenames(tmpdir)
+        # Fuse the received list of lists into one list
+        all_filenames = list(itertools.chain(*gathered_filename_lists))
 
-        gathered_filenames = MPI.COMM_WORLD.allgather(updated_filenames)
+        # Verify that all items in the list are unique
+        assert len(all_filenames) == len(set(all_filenames))
 
-        assert len(gathered_filenames) == num_ranks
+    def test_all_items_are_distributed(self, distributed_filenames):
+        gathered_filename_lists, data_dir = distributed_filenames
 
-        count = 0
-        for f_names in gathered_filenames:
-            count += len(f_names)
+        # Fuse the received list of lists into one list
+        all_filenames = list(itertools.chain(*gathered_filename_lists))
 
-        unique_filenames = set()
-        for f_names in gathered_filenames:
-            unique_filenames.add(*f_names)
+        if rank == 0:
+            # Use the un-decorated loader to get a list of filenames
+            read_filenames = os.listdir(data_dir)
 
-        assert count == len(unique_filenames)
+            # Verify that contents are the same
+            assert not (set(all_filenames) ^ set(read_filenames))
